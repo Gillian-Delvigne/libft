573b2cd5645bed171d9721a1d5fedc1d93a8401e
Gillian Delvigne <gdelvign@mi-r1-p3.s19.be>
2023-10-27 17:16:42 +0200

    strlcpy fixed

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gdelvign <gdelvign@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 10:35:29 by gdelvign          #+#    #+#             */
-/*   Updated: 2023/10/27 15:37:41 by gdelvign         ###   ########.fr       */
+/*   Updated: 2023/10/27 17:09:26 by gdelvign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	srcsize;
 
 	srcsize = ft_strlen(src);
-	if (!dst && src && dstsize < 1)
+	if (dstsize == 0)
 		return (srcsize);
 	i = 0;
-	while (i < (dstsize - 1) && src[i])
+	while (src[i] && i < (dstsize - 1))
 	{
 		dst[i] = src[i];
 		i++;
