9f4568967d171899f9245e911442cf368aaccdd3
Gillian Delvigne <gdelvign@fu-r4-p4.s19.be>
2023-10-30 16:07:31 +0100

    some functions optimization

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gdelvign <gdelvign@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 16:42:21 by gdelvign          #+#    #+#             */
-/*   Updated: 2023/10/24 18:36:16 by gdelvign         ###   ########.fr       */
+/*   Updated: 2023/10/30 15:38:54 by gdelvign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		while (len-- > 0)
 			c_dst[len] = c_src[len];
 	}
-	else
+	else if (c_dst < c_src)
 	{
 		while (i < len)
 		{
